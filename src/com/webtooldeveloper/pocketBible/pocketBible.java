a9b12cc54d1557fe35cc7c8e18a6e53ca15ccd79
Robert Flach <robert.flach@webtooldeveloper.com>
2010-08-24 12:05:53 -0500

    Adding initial directory structure

---
+++ src/com/webtooldeveloper/pocketBible/pocketBible.java
@@ -0,0 +1,13 @@
+package com.webtooldeveloper.pocketBible;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class pocketBible extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
